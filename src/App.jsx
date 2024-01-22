cbd37f42f588ca6de084497bb3e6b8e5def2a444
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-22 17:38:54 -0300

    se creo componente TituloProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import TituloProps from './components/TituloProps';
+import Container from 'react-bootstrap/Container';
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-    </>
+    <Container>
+      <TituloProps></TituloProps>
+    </Container>
+      
+    
   )
 }
 
---
+++ src/components/TituloProps.jsx
@@ -0,0 +1,9 @@
+const TituloProps = () => {
+    return (
+        <h1>
+           Hello  
+        </h1>
+    );
+};
+
+export default TituloProps;
